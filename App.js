8d55198c73494d0f3bf7730bc3a9c6ce5076c941
ShadowChaser <30345922+ShadowChaser@users.noreply.github.com>
2018-05-02 21:44:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './Components/Common';
+import LoginForm from './Components/LoginForm';
+
+
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAh4C7nlrT9o4EMaIApEH749j_xoKh16g0',
+    authDomain: 'authentication-82409.firebaseapp.com',
+    databaseURL: 'https://authentication-82409.firebaseio.com',
+    projectId: 'authentication-82409',
+    storageBucket: 'authentication-82409.appspot.com',
+    messagingSenderId: '1006267421309'
+  });
+
+
+class App extends Component {
+  state = { loggedIn: null };
+  componentWillMount() {
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (<Button omPredd={()=> firebase.auth().signOut()}>
+            Log Out
+            </Button>);
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
